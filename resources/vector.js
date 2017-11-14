52c63d1c6ea998dc2dfcbb712b2cd3e67d81fdc3
sean-codes <sean_codes@outlook.com>
2017-11-13 22:28:36 -0600

    few more functions

--- resources/vector.js
+++ resources/vector.js
@@ -4,17 +4,39 @@ class Vector {
       this.y = y
    }
 
-   add(vect){
+   add(vect) {
       this.x += vect.x
       this.y += vect.y
-   }
-   
-   scale(amount){
-      this.x *= amount
-      this.y *= amount
+      return this
    }
 
-   reflect(){
+   scale(amount) {
+      this.x *= amount
+      this.y *= amount
+      return this
+   }
+
+   dot(vect) {
+      return this.x * vect.x + this.y * vect.y
+   }
+
+   cross() {
+
+   }
+
+   normalize() {
+      return this.scale(1/this.length())
+   }
+
+   length() {
+      return Math.sqrt(this.x*this.x + this.y*this.y)
+   }
+
+   reflect() {
       this.scale(-1)
+   }
+
+   clone() {
+      return new Vector(this.x, this.y)
    }
 }
