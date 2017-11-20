2abb11db68ea42b7c305ade2c88116e87e3cc05c
sean-codes <sean_codes@outlook.com>
2017-11-19 22:56:24 -0600

    Switch to only points

--- resources/shapes.js
+++ resources/shapes.js
@@ -1,6 +1,6 @@
 class Shape {
-   constructor(x, y){
-      this.pos = new Vector(x, y)
+   constructor(){
+      this.points = []
       this.direction = new Vector(0, 0)
       this.sides = []
    }
@@ -16,10 +16,8 @@ class Shape {
    }
 
    move() {
-      this.pos.add(this.direction)
-      for(var side of this.sides) {
-         side.p1.add(this.direction)
-         side.p2.add(this.direction)
+      for(var point of this.points) {
+         point.add(this.direction)
       }
    }
 
@@ -33,45 +31,50 @@ class Shape {
 
    draw(ctx) {
       ctx.beginPath()
-      ctx.moveTo(this.pos.x, this.pos.y)
       // Line to each side
-      for(var side of this.sides){
-         ctx.lineTo(side.p2.x, side.p2.y)
+      for(var point of this.points){
+         ctx.lineTo(point.x, point.y)
       }
 
+      ctx.closePath()
       ctx.stroke()
    }
 }
 
 class Line extends Shape {
    constructor(x, y, x2, y2){
-      super(x, y)
+      super()
 
-      this.sides = [
-         { p1: new Vector(x, y), p2: new Vector(x2, y2) }
+      this.points = [
+         new Vector(x, y),
+         new Vector(x2, y2)
       ]
    }
 }
 
 class Square extends Shape {
    constructor(x, y, size){
-      super(x, y)
+      super()
 
       this.width = size
       this.height = size
-      this.sides = [ // Top, Left, Bottom, Right
-         { p1: new Vector(this.pos.x, this.pos.y), p2: new Vector(this.pos.x+this.width, this.pos.y) },
-         { p1: new Vector(this.pos.x+this.width, this.pos.y), p2: new Vector(this.pos.x+this.width, this.pos.y+this.height) },
-         { p1: new Vector(this.pos.x+this.width, this.pos.y+this.height), p2: new Vector(this.pos.x, this.pos.y + this.height) },
-         { p1: new Vector(this.pos.x, this.pos.y+this.height), p2: new Vector(this.pos.x, this.pos.y) },
+
+      this.points = [
+         new Vector(x, y),
+         new Vector(x+this.width, y),
+         new Vector(x+this.width, y+this.height),
+         new Vector(x, y+this.height)
       ]
    }
 }
 
 class Circle extends Shape{
    constructor(x, y, radius) {
-      super(x, y)
+      super()
       this.radius = radius
+      this.points = [
+         new Vector(x, y)
+      ]
    }
 
    move() {
@@ -80,7 +83,7 @@ class Circle extends Shape{
 
    draw(ctx) {
       ctx.beginPath()
-      ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2*Math.PI);
+      ctx.arc(this.points[0].x, this.points[0].y, this.radius, 0, 2*Math.PI);
       ctx.stroke()
    }
 }
