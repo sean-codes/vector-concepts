b78ca424d6b9204fe9a5d88c3c8a95ab6e116c52
sean-codes <sean_codes@outlook.com>
2017-11-19 22:56:48 -0600

    ☠️

--- experiments/unitcircle.js
+++ experiments/unitcircle.js
@@ -1,15 +1,24 @@
 var scene = new Scene(document.querySelector('canvas'))
-
-// Create and Step event
-scene.create = function(){
-   var circle = new Circle(this.width/2, this.height/2, 50)
-   this.addShape(circle)
-}
+var circle = new Circle(scene.width/2, scene.height/2, 50)
+var line = new Line(scene.width/2, scene.height/2, scene.width/2, scene.height/2)
+scene.addShape(circle)
+scene.addShape(line)
 
 scene.step = function(){
    scene.clear()
 
    for(var shape of scene.shapes){
+
+      if(shape.constructor.name == 'Line') {
+         // Get the angle between horizontal line and center - mousepos
+         var horizontal = new Vector(1, 0)
+         var vertical = new Vector(0, 1)
+         var center = new Vector(this.width/2, this.height/2)
+         var mousedir = this.mousepos.clone().min(center).normalize()
+
+         console.log(Math.cos(mousedir), Math.sin(mousedir))
+
+      }
       scene.drawShape(shape)
    }
 }
