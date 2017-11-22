cf210504a5fca60a5fa64dbaf500f83474f2d150
sean-codes <sean_codes@outlook.com>
2017-11-22 07:58:28 -0600

    tan

--- experiments/unitcircle.js
+++ experiments/unitcircle.js
@@ -10,6 +10,7 @@ var circleUnit = new Circle(vCenter.x, vCenter.y, 50)
 var circleSin = new Circle(vCenter.x, vCenter.y, 3)
 var circleCos = new Circle(vCenter.x, vCenter.y, 3)
 var circleTan = new Circle(vCenter.x, vCenter.y, 3)
+var circleCTan = new Circle(vCenter.x, vCenter.y, 3)
 circleTan.colorFill = colorBlue
 circleCos.colorFill = colorRed
 circleSin.colorFill = colorGreen
@@ -18,6 +19,7 @@ scene.addShape(circleUnit)
 scene.addShape(circleSin)
 scene.addShape(circleCos)
 scene.addShape(circleTan)
+scene.addShape(circleCTan)
 
 scene.step = function(){
    scene.clear()
@@ -31,6 +33,12 @@ scene.step = function(){
    circleSin.points[0].y = mouseDir.y * 50 + vCenter.y
    circleCos.points[0].x = mouseDir.x * 50 + vCenter.x
 
+   // Move Tan
+   circleTan.points[0].x = 50 + vCenter.x
+   circleTan.points[0].y = Math.tan(mouseDirDegree / (180/Math.PI)) * 50 + vCenter.y
+   // Move CTan
+   circleCTan.points[0].x = (1/Math.tan(mouseDirDegree / (180/Math.PI))) * 50 + vCenter.x
+   circleCTan.points[0].y = 50 + vCenter.y
    // Draw angle
    this.ctx.beginPath()
    this.ctx.arc(vCenter.x, vCenter.y, 30, 0, mouseDirDegree / (180/Math.PI), false)
@@ -48,7 +56,9 @@ scene.step = function(){
 	this.ctx.fillStyle = colorBlack
    this.ctx.fillText('cos: ' + Math.round(mouseDir.x*1000)/1000, 10, 20)
    this.ctx.fillText('sin: ' + Math.round(mouseDir.y*1000)/1000, 10, 40)
-   this.ctx.fillText('dir: ' + Math.round(mouseDirDegree*1000)/1000, 10, 60)
+   this.ctx.fillText('tan: ' + Math.round(Math.tan(mouseDirDegree / (180/Math.PI))*1000)/1000, 10, 60)
+   this.ctx.fillText('ctan: ' + Math.round(-1/Math.tan(mouseDirDegree / (180/Math.PI))*1000)/1000, 10, 80)
+   this.ctx.fillText('dir: ' + Math.round(mouseDirDegree*1000)/1000, 10, 100)
 }
 
 // Startup!
