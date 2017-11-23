4d6f73329508cb6d31e1b3246a1933e45090fd15
sean-codes <sean_codes@outlook.com>
2017-11-23 00:23:42 -0600

    left and right keys

--- experiments/ship.js
+++ experiments/ship.js
@@ -1,15 +1,17 @@
 var scene = new Scene(document.querySelector('canvas'))
 var ship = new Triangle(scene.width/2, scene.height/2, 25, 25)
-
 scene.addShape(ship)
 scene.step = function(){
+   scene.clear()
    for(var shape of scene.shapes){
       scene.drawShape(shape)
    }
 
    if(scene.keys[39]){
-      console.log('right arrow')
-      ship.rotate(1, new Vector(scene.width/2, scene.height/2))
+      ship.rotate(2, new Vector(scene.width/2, scene.height/2))
+   }
+   if(scene.keys[37]){
+      ship.rotate(-2, new Vector(scene.width/2, scene.height/2))
    }
 }
 scene.start();
