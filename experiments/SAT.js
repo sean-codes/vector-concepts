dbf89a7daebffb51f07796b349c917c1bab6d322
sean-codes <sean_codes@outlook.com>
2017-11-26 14:12:22 -0600

    updated version 👩‍🚀

--- experiments/SAT.js
+++ experiments/SAT.js
@@ -1,70 +1,60 @@
 var scene = new Scene()
+scene.addShape(new Square(scene.width/2, scene.height/2, 50))
+scene.addShape(new Square(scene.width/2-110, scene.height/2, 50))
+scene.addShape(new Triangle(scene.width/2 - 35, scene.height/2+20, 50))
 
-var vCenter = new Vector(scene.width/2, scene.height/2)
-var boxes = [
-   new Square(vCenter.x, vCenter.y, 50),
-   new Square(vCenter.x+75, vCenter.y, 50),
-   new Triangle(vCenter.x-75, vCenter.y, 50),
-]
-
-scene.addShape(boxes[0])
-scene.addShape(boxes[1])
-scene.addShape(boxes[2])
-
-
-scene.step = function(){
-   scene.debug('Hold mouse down to move shape')
+scene.step = function() {
    scene.drawShapes()
-   boxes[0].rotate(0.25)
-   for(var shape1 of scene.shapes){
-      var touch = sat(shape1)
-      shape1.colorStroke = touch ? '#F22' : '#000';
+
+   // Loop each shape. Check for a collision
+   for(var shape of scene.shapes) {
+      shape.colorStroke = sat(shape) ? '#F22' : '#000'
    }
 
-   if(scene.mouse.down) {
-      scene.keys[32]
-         ? boxes[0].setPos(scene.mouse.pos)
-         : boxes[1].setPos(scene.mouse.pos)
+   if(scene.mouse.down){
+      scene.shapes[2].setPos(scene.mouse.pos)
    }
 
+   scene.shapes[1].rotate(1)
+   scene.debug('Click and hold to set triangle position!')
 }
 
+function sat(check) {
 
-function sat(checkShape) {
-   var checkAxis = checkShape.axis()
    // Loop each shape
    for(var shape of scene.shapes) {
-      if(shape.unique == checkShape.unique) continue;
-      // Check each Axis
-      var axisArr = shape.axis().concat(checkAxis)
+      if(shape.unique == check.unique) continue
+      // Gather all the AXIS
+      var axis = shape.axis().concat(check.axis())
       var touch = true
-      for(var axis of axisArr) {
-         // Pairs
-         var shapeMinMax = {x: undefined, y: undefined}
-         var checkMinMax = {x: undefined, y: undefined}
 
-         // Check current shape
-         for(var point of shape.points){
-            var dot = point.dot(axis)
-            shapeMinMax.min = shapeMinMax.min ? Math.min(shapeMinMax.min, dot) : dot
-            shapeMinMax.max = shapeMinMax.max ? Math.max(shapeMinMax.max, dot) : dot
+      for(var ax of axis) {
+         var checkMin = undefined
+         var checkMax = undefined
+         var shapeMin = undefined
+         var shapeMax = undefined
+         // Gather the Min/Max
+         for(var point of shape.points) {
+            var dot = point.dot(ax)
+            shapeMin = shapeMin ? Math.min(shapeMin, dot) : dot
+            shapeMax = shapeMax ? Math.max(shapeMax, dot) : dot
          }
 
-         // Check the checkshape on the axis
-         for(var point of checkShape.points){
-            var dot = point.dot(axis)
-            checkMinMax.min = checkMinMax.min ? Math.min(checkMinMax.min, dot) : dot
-            checkMinMax.max = checkMinMax.max ? Math.max(checkMinMax.max, dot) : dot
+         for( var point of check.points) {
+            var dot = point.dot(ax)
+            checkMin = checkMin ? Math.min(checkMin, dot) : dot
+            checkMax = checkMax ? Math.max(checkMax, dot) : dot
          }
 
-         // Return if no touch
-         if(checkMinMax.min > shapeMinMax.max || checkMinMax.max < shapeMinMax.min){
+         // Check for collision
+         if(checkMin > shapeMax || checkMax < shapeMin) {
             touch = false
             break
          }
       }
-      if(!touch){ continue }
+      if(!touch) continue
       return true
    }
+
    return false
 }
