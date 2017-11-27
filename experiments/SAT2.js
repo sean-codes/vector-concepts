1f319e1f8c41214e67c56caba22e5bb6ae00c2a9
sean-codes <sean_codes@outlook.com>
2017-11-26 22:29:14 -0600

    multiple shapes

--- experiments/SAT2.js
+++ experiments/SAT2.js
@@ -1,5 +1,7 @@
 var scene = new Scene()
+var heldShape = 0
 scene.addShape(new Square(scene.width/2 + 30, scene.height/2, 50))
+scene.addShape(new Square(scene.width/2 - 30, scene.height/2, 50))
 scene.addShape(new Square(scene.width/2 - 30, scene.height/2, 50))
 
 
@@ -8,12 +10,19 @@ scene.step = function() {
    scene.drawShapes()
 
    // Check for collision
-   sat(scene.shapes[0], scene.shapes[1])
+   for(var s1 of scene.shapes) {
+      for(var s2 of scene.shapes) {
+         if(s1.unique != s2.unique) sat(s1, s2)
+      }
+   }
 
    // Move Shape
    if(scene.mouse.down) {
       var pos = scene.mouse.pos.clone().add(new Vector(-25, -25))
       scene.shapes[0].setPos(pos)
+   }
+   if(scene.keys[37]){
+      scene.shapes[0].move(new Vector(-1, 0))
    }
 }
 
