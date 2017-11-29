73d1c2d764edc5e5a98240de3e51658a5bb9be1a
sean_codes <sean_codes@outlook.com>
2017-11-29 17:00:01 -0600

    parametric

--- experiments/intersectionParametric.js
+++ experiments/intersectionParametric.js
@@ -1,7 +1,7 @@
 var scene = new Scene()
 
-var line1 = new Line(200, 100, 300, 200)
-var line2 = new Line(300, 100, 200, 200)
+var line1 = new Line(100, 100, 300, 200)
+var line2 = new Line(100, 200, 200, 200)
 
 scene.addShape(line1)
 scene.addShape(line2)
@@ -11,47 +11,81 @@ scene.addShape(circle)
 
 scene.step = function() {
    scene.drawShapes()
-	var ipoint = intersect(line1, line2)
+	var ipoint = intersect(line1.points[0], line1.points[1], line2.points[0], line2.points[1])
+	console.log(ipoint)
 	if(ipoint) circle.setPos(ipoint)
 
 	if(scene.mouse.down){
-		line2.points[0].copy(scene.mouse.pos)
+		line2.points[1].copy(scene.mouse.pos)
 	}
 }
 
-// Standard Form
-// function lineIntersection(l1, l2){
-//    var l1c =
-//    var l1a =
-//    var l1b =
-//
-//    var l2c =
-//    var l2a =
-//    var l2b =
-//
-//    var denomenator =
-//    if(!denomenator) return false
-//    var x =
-//    var y =
-// }
+function intersect(Start1, End1, Start2, End2) {
+	// [Operation] - Learn how to find point on a line
+	//---------------------------------------------------------------------------------------------------
+	// Equation of a line
+	// Line = StartPoint + (EndPoint - StartPoint) * T
+	// Line = StartPoint + Direction * T
+	// T is a ratio from 0 - 1. 0 = StartPoint and 1 = EndPoint. > 0 and < 1 is everything in between
 
-// Slope intercept
-function intersect(line1, line2) {
-	line1.p1 = line1.points[0]
-	line1.p2 = line1.points[1]
-	line2.p1 = line2.points[0]
-	line2.p2 = line2.points[1]
-   //y = mx+b and then dance
-   var s1 = (line1.p1.y - line1.p2.y) / (line1.p1.x - line1.p2.x)
-   var s2 = (line2.p1.y - line2.p2.y) / (line2.p1.x - line2.p2.x)
+	// You can find out if a line intersects if they equal each other
+	// StartPoint1 + Direction1 * T1 == StartPoint2 + Direction2 * T2
 
-	// Slops are the same
-   if (s1 == s2) return undefined
-   var b1 = -s1 * line1.p1.x + line1.p1.y
-   var b2 = -s2 * line2.p1.x + line2.p1.y
+	// This is parametric and will give us 2 equations
+	// 1. Start1.x + Direction1.x * T1 == Start2.x + Direction2.y * T2
+	// 2. Start1.y + Direction1.y * T1 == Start2.y + Direction2.y * T2
 
+	// [Mission 1] We need to isolate T1 for both equations
+	// 1. T1 = (Start2.x + Direction2.x * T2 - Start1.x)/Direction1.x
+	// 1. T1 = (Start2.y + Direction2.y * T2 - Start1.y)/Direction1.y
 
-   var x = (b2 - b1) / (s1 - s2)
-   var y = s1 * x + b1
-   return new Vector(x, y)
+	// Now we got rid of T1 lets rewrite this with only 1 unknown. Mister T2!
+	// (Start2.x + Direction2.x * T2 - Start1.x)/Direction1.x == (Start2.y + Direction2.y * T2 - Start1.y)/Direction1.y
+
+	// [Mission 2] We need to figure out how to calculate T2.
+	// 1. Multiply the left side by Direction1.y
+	// 2. Multiply the right side by Direction1.x
+	// (Start2.x + Direction2.x * T2 - Start1.x) * Direction1.y == (Start2.y + Direction2.y * T2 - Start1.y) * Direction1.x
+
+	// 3. Distribute! hint: THIS IS GOING TO GET SO SCAREY!!! :]
+	// Start2.x*Direction1.y + Direction2.x*Direction1.y*T2 - Start1.x*Direction1.y == Start2.y*Direction1.x + Direction2.y*Direction1.x*T2 - Start1.y*Direction1.x
+
+	// 4. This is where I tip over!! Move everything around so your T2 are on the same side
+	// Direction2.x*Direction1.y*T2 - Direction2.y*Direction1.x*T2 == Start2.y*Direction1.x - Start1.y*Direction1.x - Start2.x*Direction1.y + Start1.x*Direction1.y
+
+	// 5. On the left side we see that spooky T2 and be undistributed
+	// T2(Direction2.x*Direction1.y - Direction2.y*Direction1.y) == Start2.y*Direction1.x - Start1.y*Direction1.x - Start2.x*Direction1.y + Start1.x*Direction1.y
+
+	// 6. We made it! Move those others over. USE PARENTHESIS HERE!!!
+	// T2 = (Start2.y*Direction1.x - Start1.y*Direction1.x - Start2.x*Direction1.y + Start1.x*Direction1.y)/(Direction2.x*Direction1.y - Direction2.y*Direction1.y)
+
+	// [Mission 3] With T2 we can quickly get T1 :]
+	// T1 = Start2.x + Direction2.x * T2 - Start1.x)/Direction.x
+
+	// [Mission 4] If They are both within 0 to 1 the touch!
+	// [Mission 5] Drop T1 or T2 one of the lines equation to get the point
+	// intersectionX = Start1.x + Direction.x * T1
+	// intersectionY = Start1.y + Direction.y * T1
+
+	// To actually code this we start with getting the directions
+	var Direction1 = {
+		x: End1.x - Start1.x,
+		y: End1.y - Start1.y
+	}
+	var Direction2 = {
+		x: End2.x - Start2.x,
+		y: End2.y - Start2.y
+	}
+
+	// Use the math we created.
+	// I ALWAYS MISTYPE SOMETHING HERE
+	var T2 = (Start2.y*Direction1.x - Start1.y*Direction1.x - Start2.x*Direction1.y + Start1.x*Direction1.y)/(Direction2.x*Direction1.y - Direction2.y*Direction1.y)
+	var T1 = (Start2.x + Direction2.x * T2 - Start1.x)/Direction1.x
+
+	console.log(T2, T1)
+	if(T2 < 0 || T2 > 1 || T1 < 0 || T1 > 1){ return false }
+	return new Vector(
+		Start1.x + Direction1.x * T1,
+		Start1.y + Direction1.y * T1
+	)
 }
