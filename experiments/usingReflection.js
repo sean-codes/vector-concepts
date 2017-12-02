a7c6cfe2c0df5570bbe9c04d4cb09642d5292565
sean-codes <sean_codes@outlook.com>
2017-12-02 17:53:17 -0600

    not sure :]

--- experiments/usingReflection.js
+++ experiments/usingReflection.js
@@ -1,48 +1,66 @@
 var scene = new Scene()
-scene.addShape(new Square(0, 0, scene.width-150, scene.height))
+// Walls
+scene.addShape(new Line(scene.width-10, scene.height-10, scene.width - 10, 10))
+scene.addShape(new Line(10, 10, 10, scene.height-10))
+scene.addShape(new Line(10, scene.height-10, scene.width-10, scene.height-10))
+scene.addShape(new Line(scene.width-10, 10, 10, 10))
+//scene.addShape(new Square(0, 0, scene.width-150, scene.height))
 scene.addShape(new Square(scene.width/2, scene.height/2, 50))
-scene.addShape(new Square(scene.width/2 - 100, scene.height/2, 50))
-// scene.addShape(new Square(scene.width/2 - 100, scene.height/2- 80, 50))
-// scene.addShape(new Square(scene.width/2 + 100, scene.height/2- 80, 50))
-var circle = new Circle(50, 50, 5)
-circle.colorFill = '#F22'
+scene.addShape(new Square(scene.width/2 - 100, scene.height/2+20, 50))
+scene.addShape(new Square(scene.width/2 - 100, scene.height/2- 70, 50))
+scene.addShape(new Square(scene.width/2 + 100, scene.height/2- 70, 50))
+var circles = [
+   new Circle(243.5333, 115.012, 5),
+   new Circle(200, 115.012, 5),
+   new Circle(0.5333, 115.012, 5),
+   new Circle(243.5333, 115.012, 5),
+   new Circle(243.5333, 115.012, 5)
+]
+//circle.colorFill = '#F22'
+var circleDir = new Vector(-2, 3)
 
-var circleDir = new Vector(2, 2)
-scene.addShape(circle)
+scene.addShapes(circles)
 scene.step = function() {
+   //scene.shapes[2].rotate(2)
    scene.drawShapes()
-   //scene.shapes[4].rotate(3)
-   moveCircle()
-
+   scene.shapes[4].rotate(3)
+   for(var circle of circles){
+      moveCircle(circle)
+   }
 }
 
-function moveCircle() {
+function moveCircle(circle) {
+   if(!circle.dir) circle.dir = new Vector(Math.random()*2+1, Math.random()*2+1)
+   circle.colorFill = '#F22'
    for(var shape of scene.shapes) {
       // Only colliding on squares right now!
-      if(shape.type != 'Square') continue
+      if(shape.type == 'Circle') continue
 
       for(var side of shape.sides()) {
          sideDir = side[0].clone().min(side[1])
-         circlePos = circle.points[0].clone().add(circleDir)
-         circlePosRelative = side[0].clone().min(circlePos)
-         scene.debug(circlePosRelative + ' ' + circlePosRelative.dot(sideDir))
-         var facing = circlePosRelative.dot(sideDir)
+         var centerOfSide = side[0].clone().min(sideDir.clone().scale(0.5))
+         var crossOfSide = centerOfSide.clone().add(sideDir.cross().unit().scale(-5))
+         scene.drawCircle(centerOfSide, 3)
+         scene.drawCircle(crossOfSide, 3, '#F22')
+         var sideDir = centerOfSide.min(crossOfSide)
+         var facing = sideDir.dot(circle.dir)
          // Make sure they are moving in opposite directions
+         //scene.debug(facing)
          if(facing > 0) {
             // Get distance from
             var closest = closestPointToLine(circle.points[0], side)
             var distance = circle.points[0].distance(closest)
 
-            if(distance < 5 + circleDir.length()) {
-               circle.back(circleDir)
-               circleDir = circleDir.reflect(side[1].clone().min(side[0]))
-               circle.move(circleDir)
+            if(distance < 5 + circle.dir.length()) {
+               circle.back(circle.dir)
+               circle.dir = circle.dir.reflect(side[1].clone().min(side[0]))
+               circle.move(circle.dir)
                return
             }
          }
       }
    }
-   circle.move(circleDir)
+   circle.move(circle.dir)
 }
 
 function closestPointToLine(point, linePoints){
