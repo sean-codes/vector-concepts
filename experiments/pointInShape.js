bf1bbfe1de695275408ddc34bba58465a0201658
sean-codes <sean_codes@outlook.com>
2017-12-05 22:13:10 -0600

    need to build a verlet object

---
+++ experiments/pointInShape.js
@@ -0,0 +1,50 @@
+var scene = new Scene()
+var boundary = scene.sides()
+var testVel = new Vector(2, 2)
+
+var points = [
+   new Point(10, 10, testVel)
+]
+
+scene.step = function() {
+   // Move
+   for(var point of points) {
+      point.move()
+   }
+
+   // Draw
+   for(var point of points) {
+      point.draw()
+   }
+}
+
+
+function Point(x, y, vel) {
+   this.pos = new Vector(x, y)
+   this.old = this.pos.clone().min(vel)
+
+   this.move = function() {
+      var vel = this.pos.clone().min(this.old)
+      this.old = this.pos.clone()
+      this.pos.add(vel)
+
+      this.constrain()
+   }
+
+   this.draw = function() {
+      scene.drawCircle(this.pos, 3, '#2F2')
+   }
+
+   this.constrain = function() {
+      this.vel = this.pos.clone().min(this.old)
+      if(this.pos.x > scene.width || this.pos.x < 0){
+         this.pos.x = this.pos.x < 0 ? 0 : scene.width
+         this.old.x = this.pos.x + this.vel.x
+      }
+
+      if(this.pos.y > scene.height || this.pos.y < 0){
+         this.pos.y = this.pos.y < 0 ? 0 : scene.height
+         this.old.y = this.pos.y + this.vel.y
+      }
+   }
+}
