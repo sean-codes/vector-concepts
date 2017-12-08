f3c9831cf123b071af28d1805df5398b54767ab8
sean_codes <sean_codes@outlook.com>
2017-12-08 15:24:11 -0600

    Order! 👩‍💻

--- resources/scene.js
+++ resources/scene.js
@@ -1,16 +1,20 @@
 class Scene {
+	//---------------------------------------------------------------------------
+	// Setup
+	//---------------------------------------------------------------------------
    constructor(canvas) {
       this.canvas = canvas
       if(!this.canvas){
          this.canvas = document.querySelector('canvas')
       }
       this.canvas.tabIndex = 1
-      this.ctx = this.canvas.getContext('2d')
+		this.ctx = this.canvas.getContext('2d')
 
-      this.width = this.canvas.width
-      this.height = this.canvas.height
-      this.speed = 1000/60
-      this.interval = undefined
+		this.width = this.canvas.width
+		this.height = this.canvas.height
+		this.speed = 1000/60
+		this.interval = undefined
+
       this.shapes = []
       this.debugPos = new Vector(20, 0)
       this.center = new Vector(this.width/2, this.height/2)
@@ -24,6 +28,7 @@ class Scene {
 
       this.keys = {}
       this.start()
+		this.debugQueue = []
    }
 
    listen(){
@@ -49,13 +54,16 @@ class Scene {
       })
    }
 
+	//---------------------------------------------------------------------------
+	// Loop
+	//---------------------------------------------------------------------------
    step(){}
-
    start(){
       this.interval = setInterval(() => {
          this.debugPos = this.debugPos.set(10, 0)
          this.clear()
          this.step()
+			this.drawDebugList()
 			this.mouse.up = false
       }, this.speed)
    }
@@ -67,12 +75,16 @@ class Scene {
    clear() {
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
    }
+
    setSpeed(speed) {
       this.stop()
       this.speed = speed
       this.start()
    }
 
+	//---------------------------------------------------------------------------
+	// Shapes
+	//---------------------------------------------------------------------------
    addShapes(shapes){
       for(var shape of shapes){
          this.addShape(shape)
@@ -83,7 +95,72 @@ class Scene {
       this.shapes.push(shape)
    }
 
-   drawShapes() {
+	//---------------------------------------------------------------------------
+	// Observing
+	//---------------------------------------------------------------------------
+   debug(text, color){
+      this.ctx.font = '16px Monospace'
+      this.ctx.fillStyle = color || '#222'
+      this.debugPos.add(new Vector(0, 18))
+      this.ctx.fillText(text, this.debugPos.x, this.debugPos.y)
+   }
+
+	debugLine(v1, v2, color) {
+		this.debugQueue.push({
+			type: 'Line',
+			v1: {x:v1.x, y:v1.y},
+			v2: {x:v2.x, y:v2.y},
+			color: color })
+	}
+
+	debugText(v, text, color) {
+		this.debugQueue.push({
+			type: 'Text',
+			v: {x:v.x, y:v.y},
+			text: text,
+			color: color })
+	}
+
+	debugCircle(v, radius, color) {
+		this.debugQueue.push({
+			type: 'Circle',
+			v: {x:v.x, y:v.y},
+			radius: radius,
+			color: color })
+	}
+
+	drawDebugList(){
+		this.debugQueue = this.debugQueue.filter((draw) => {
+			this[`draw${draw.type}`](draw)
+		})
+	}
+
+	//---------------------------------------------------------------------------
+	// Drawing
+	//---------------------------------------------------------------------------
+	drawLine(info) {
+      this.ctx.beginPath()
+      this.ctx.moveTo(info.v1.x, info.v1.y)
+      this.ctx.lineTo(info.v2.x, info.v2.y)
+		this.ctx.strokeStyle = info.color || '#000'
+      this.ctx.stroke()
+   }
+
+   drawCircle(info) {
+      this.ctx.beginPath()
+      this.ctx.arc(info.v.x, info.v.y, info.radius, 0, Math.PI*2)
+      this.ctx.fillStyle = info.color || 'transparent'
+      this.ctx.strokeStyle = info.color || 'transparent'
+      this.ctx.fill()
+      this.ctx.stroke()
+   }
+
+   drawText(info) {
+      this.ctx.fillStyle = info.color || '#000'
+      this.ctx.fillText(info.text, info.v.x, info.v.y)
+   }
+
+	drawShapes() {
       for(var shape of this.shapes){
          this.drawShape(shape)
       }
@@ -93,35 +170,9 @@ class Scene {
       shape.draw(this.ctx)
    }
 
-   drawLine(v1, v2, color) {
-      if(color) scene.ctx.strokeStyle = color
-      this.ctx.beginPath()
-      this.ctx.moveTo(v1.x, v1.y)
-      this.ctx.lineTo(v2.x, v2.y)
-      this.ctx.stroke()
-   }
-
-   drawCircle(v, radius, color) {
-      this.ctx.beginPath()
-      this.ctx.arc(v.x, v.y, radius, 0, Math.PI*2)
-      this.ctx.fillStyle = color || 'transparent'
-      this.ctx.strokeStyle = color || 'transparent'
-      this.ctx.fill()
-      this.ctx.stroke()
-   }
-
-   drawText(v, text, color) {
-      this.ctx.fillStyle = color || '#000'
-      this.ctx.fillText(text, v.x, v.y)
-   }
-
-   debug(text, color){
-      this.ctx.font = '16px Monospace'
-      this.ctx.fillStyle = color || '#222'
-      this.debugPos.add(new Vector(0, 18))
-      this.ctx.fillText(text, this.debugPos.x, this.debugPos.y)
-   }
-
+	//---------------------------------------------------------------------------
+	// Inputs
+	//---------------------------------------------------------------------------
    mousemove(x, y) {
       this.mouse.pos.setX(x)
       this.mouse.pos.setY(y)
@@ -144,6 +195,9 @@ class Scene {
       this.keys[code] = false
    }
 
+	//---------------------------------------------------------------------------
+	// Utility
+	//---------------------------------------------------------------------------
    randomColor() {
       var r = Math.floor(Math.random()*50)
       var g = Math.floor(Math.random()*75)
